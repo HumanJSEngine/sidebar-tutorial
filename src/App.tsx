0b3a621322f16383c96aa71a65ce9cd8d701715b
bmj <bmj44571627@gmail.com>
2023-03-08 21:34:17 +0900

    커밋

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Sidebar from './components/Sidebar'
+import {BrowserRouter as Router} from 'react-router-dom'; 
+
+function App() {
+
+  return (
+    <Router>
+      <Sidebar />
+    </Router>
+  )
+}
+
+export default App
